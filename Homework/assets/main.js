2d64060e4df01c8046d3e91243b84f660319a0bb
ilhambagirov <ilhambb@code.edu.az>
2021-07-05 15:30:37 -0700

    (done)

---
+++ Homework/assets/main.js
@@ -0,0 +1,12 @@
+var name = prompt("Firstname");
+
+var surname = prompt("Lastname");
+
+var age = prompt("Age");
+
+if(age<18){
+    alert("you cannot access this page")
+}
+else{
+    alert("you have access this page")
+}
